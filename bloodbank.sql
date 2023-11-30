121ea78cdada3894a66091916ffaf43fc81bfa8c
Your Name <you@example.com>
2023-12-01 01:18:21 +0530

    Final commit

--- bloodbank.sql
+++ bloodbank.sql
@@ -1,20 +1,21 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 04, 2016 at 01:05 AM
--- Server version: 5.6.17
--- PHP Version: 5.5.12
+-- Generation Time: Nov 30, 2023 at 08:46 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `bloodbank`
@@ -23,36 +24,13 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `advertisement`
---
-
-CREATE TABLE IF NOT EXISTS `advertisement` (
-  `adv_id` int(100) NOT NULL AUTO_INCREMENT,
-  `camp_title` varchar(100) NOT NULL,
-  `org_by` varchar(100) NOT NULL,
-  `pic` varchar(700) NOT NULL,
-  `detail` varchar(900) NOT NULL,
-  PRIMARY KEY (`adv_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
-
---
--- Dumping data for table `advertisement`
---
-
-INSERT INTO `advertisement` (`adv_id`, `camp_title`, `org_by`, `pic`, `detail`) VALUES
-(4, 'Ramgarhia Engg Collage', 'Ramgarhia education consial', '2.jpg', 'Blood donation camp Organised by REC.  One who donate get certificate by REC\r\nThat can help you also to gets blood in jeopard time');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `bloodgroup`
 --
 
-CREATE TABLE IF NOT EXISTS `bloodgroup` (
-  `bg_id` int(100) NOT NULL AUTO_INCREMENT,
-  `bg_name` varchar(100) NOT NULL,
-  PRIMARY KEY (`bg_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=21 ;
+CREATE TABLE `bloodgroup` (
+  `bg_id` int(100) NOT NULL,
+  `bg_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `bloodgroup`
@@ -74,26 +52,25 @@ INSERT INTO `bloodgroup` (`bg_id`, `bg_name`) VALUES
 -- Table structure for table `camp`
 --
 
-CREATE TABLE IF NOT EXISTS `camp` (
-  `camp_id` int(100) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `camp` (
+  `camp_id` int(100) NOT NULL,
   `camp_title` varchar(500) NOT NULL,
   `organised_by` varchar(500) NOT NULL,
   `state` int(100) NOT NULL,
   `city` int(100) NOT NULL,
   `pic` varchar(900) NOT NULL,
-  `detail` varchar(1000) NOT NULL,
-  PRIMARY KEY (`camp_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+  `detail` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `camp`
 --
 
 INSERT INTO `camp` (`camp_id`, `camp_title`, `organised_by`, `state`, `city`, `pic`, `detail`) VALUES
-(1, 'Ramgarhia Engg Collage', 'Ramgarhia Counsil', 1, 1, 'blood_donation_camp.jpg', 'A Blood Donation Camp at Ramgarhia Engg Collage organized by Ramgarhia consial , Phagwara.'),
-(7, ' Lovely Professional University', 'Lovely Professional University', 1, 7, 'B.D camp 034.jpg', 'A Blood Donation Camp at Lovely Professional University organized by Lovely Professional University , Jalandhar.'),
-(8, 'Guru Nanak College', 'Lions Club', 1, 1, 'p36.jpg', 'A Blood Donation Camp at G.N.C College, Phagwara organized by Lions Club, Phagwara.'),
-(9, 'Apee Jay College', 'Human Welfare Society', 1, 1, 'p38_2.jpg', 'A Blood Donation Camp at Apee-Jay College, Jalandhar organized by Human Welfare Society, Jalandhar.\n ');
+(3, 'Shri Vaishnav Vidyapeeth Vishwavidyalaya', 'Vaishnav Group', 3, 1, 'blood_donation_camp.jpg', 'A Blood Donation Camp at Shri Vaishnav Vidyapeeth Vishwavidyalaya organized by Vaishnav Group , Indore.'),
+(7, ' Medicaps College', 'Medicaps University', 3, 1, 'B.D camp 034.jpg', 'A Blood Donation Camp at  Medicaps College organized by  Medicaps University , Indore\r\n.'),
+(8, 'Guru Nanak College', 'Lions Club', 1, 7, 'p36.jpg', 'A Blood Donation Camp at G.N.C College, Phagwara organized by Lions Club, Phagwara.'),
+(9, 'Apee Jay College', 'Human Welfare Society', 1, 7, 'p38_2.jpg', 'A Blood Donation Camp at Apee-Jay College, Jalandhar organized by Human Welfare Society, Jalandhar.\n ');
 
 -- --------------------------------------------------------
 
@@ -101,21 +78,20 @@ INSERT INTO `camp` (`camp_id`, `camp_title`, `organised_by`, `state`, `city`, `p
 -- Table structure for table `city`
 --
 
-CREATE TABLE IF NOT EXISTS `city` (
-  `city_id` int(100) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `city` (
+  `city_id` int(100) NOT NULL,
   `city_name` varchar(100) NOT NULL,
   `pin_code` varchar(100) NOT NULL,
   `district` varchar(100) NOT NULL,
-  `state` int(100) NOT NULL,
-  PRIMARY KEY (`city_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+  `state` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `city`
 --
 
 INSERT INTO `city` (`city_id`, `city_name`, `pin_code`, `district`, `state`) VALUES
-(1, 'phagwara', '144401', 'kapurthala', 1),
+(1, 'Indore', '452013', 'Indore', 3),
 (4, 'faridabad', '121001', 'Gurgaon', 2),
 (7, 'jalandhar', '144001', 'jalandhar', 1);
 
@@ -125,14 +101,13 @@ INSERT INTO `city` (`city_id`, `city_name`, `pin_code`, `district`, `state`) VAL
 -- Table structure for table `contacts`
 --
 
-CREATE TABLE IF NOT EXISTS `contacts` (
-  `row_id` int(100) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `contacts` (
+  `row_id` int(100) NOT NULL,
   `name` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `mobile` varchar(100) NOT NULL,
-  `subj` varchar(700) NOT NULL,
-  PRIMARY KEY (`row_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+  `subj` varchar(700) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `contacts`
@@ -151,8 +126,8 @@ INSERT INTO `contacts` (`row_id`, `name`, `email`, `mobile`, `subj`) VALUES
 -- Table structure for table `donarregistration`
 --
 
-CREATE TABLE IF NOT EXISTS `donarregistration` (
-  `donar_id` int(100) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `donarregistration` (
+  `donar_id` int(100) NOT NULL,
   `name` varchar(100) NOT NULL,
   `gender` varchar(100) NOT NULL,
   `age` varchar(100) NOT NULL,
@@ -160,33 +135,26 @@ CREATE TABLE IF NOT EXISTS `donarregistration` (
   `b_id` int(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `pwd` int(100) NOT NULL,
-  `pic` varchar(1000) NOT NULL,
-  PRIMARY KEY (`donar_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=21 ;
+  `pic` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `donarregistration`
 --
 
 INSERT INTO `donarregistration` (`donar_id`, `name`, `gender`, `age`, `mobile`, `b_id`, `email`, `pwd`, `pic`) VALUES
-(3, 'neeru', 'female', '20', '9463958058', 13, 'neeru.bawa@yahoo.com', 123, 'neeru.jpg'),
-(4, 'Paras Bhatia', 'male', '21', '9888961990', 13, 'parasbhatia08@gmail.com', 123, '11168037_1610636085842033_904443745256932191_n.jpg'),
-(5, 'Rakesh', 'male', '22', '9876505652', 17, 'rakesh12@gmil.com', 123, '1002679_629448530471361_5912528223898153120_n.jpg'),
-(6, 'Manu', 'female', '20', '9779730479', 13, 'manpreetkaler@yahoo.com', 123, '12144826_691191231017304_7118038794667291151_n.jpg'),
-(7, 'Sukhwinder', 'male', '22', '01823280290', 17, 'sunnysuyan@gmail.com', 123, 'p3Penguins.jpg'),
-(8, 'Abhishek', 'male', '24', '0123456780', 19, 'abhi@ymail.com', 123, 'resizedBlood donation (1).jpg'),
-(9, 'Ramanjeet Kaur', 'female', '26', '9295769630', 15, 'rbawa08@yahoo.com', 123, '11261437_1624099511137421_8482759699813102898_n.jpg'),
-(10, 'kuldip Banga', 'male', '26', '9878967543', 15, 'kbanga@yahoo.com', 123, '10593057_728311113906338_1063644592728298376_n.jpg'),
-(11, 'Jaspinder', 'male', '24', '9445678765', 16, 'singhjaspinder12@gmail.com', 123, '12038392_969929529729736_1007747926670734265_n (1).jpg'),
-(12, 'Sahil', 'male', '20', '8591824296', 13, 'sahildubey@gmail.com', 123, '12038380_834746969972073_6382623771453128938_n.jpg'),
-(13, 'sonu', 'male', '25', '9594918765', 16, 'sonukhana34@gmail.com', 123, '11863382_1032998280067097_2552428582935820453_n.jpg'),
-(14, 'Vinny', 'male', '24', '01824230721', 18, 'vinny786@gmail.com', 123, '11175046_366978330169273_8044521424217911178_n.jpg'),
-(15, 'Rishav Bhatia', 'male', '19', '8781846758', 19, 'rbhatia@ymail.com', 123, '1491626_647476172014342_6155162312561723489_n.jpg'),
-(16, 'Rahul Bangar', 'male', '25', '9216291294', 20, 'raulban@gmail.com', 123, '11899909_948967995160204_759838727467883977_n.jpg'),
-(17, 'Prabhjot', 'male', '24', '9818134576', 20, 'prabh786@gmail.com', 123, '11899866_690854484348510_8725848025714675727_n.jpg'),
-(18, 'Ridhima', 'female', '25', '9889786545', 14, 'ridhi@ymail.com', 123, '1.jpg'),
-(19, 'Preet karanq', 'female', '28', '8427429079', 14, 'preet22@yahoo.com', 123, '123.jpg'),
-(20, 'mntrtgrt', 'female', '35', '9888976570', 18, 'manat@yahoo.com', 123, 'ccccc.jpg');
+(22, 'Raghav \r\n', 'male', '20', '98623549720', 13, 'abc@gmail.com', 123, '1.jpg'),
+(23, 'Sambhav ', 'male', '20', '7898221970', 13, 'sambhavkanugo03@gmail.com', 123, '2.jpg'),
+(24, 'Shreyash ', 'male', '20', '7895632490', 17, 'Shreyash@gmail.com', 123, '3.jpg'),
+(25, 'Priyanshu', 'male', '19', '7869995324', 20, 'priyanshu@gmail.com', 123, '4.jpg'),
+(26, 'Vansh', 'male', '20', '982654162', 19, 'vansh@gmail.com', 123, '5.jpg'),
+(27, 'Madhav', 'male', '19', '9826054561', 17, 'madhav@gmail.com', 123, '6.jpg'),
+(28, 'Ankur', 'male', '21', '89564231090', 14, 'ankur@gmail.com', 123, '7.jpg'),
+(29, 'priyanshu', 'male', '20', '8349421007', 15, 'priyanshus@gmail.com', 123, '8.jpg'),
+(30, 'Tanay', 'male', '21', '98656321470', 16, 'tanay@gmail.com', 123, '11.jpg'),
+(31, 'Shrey', 'male', '18', '993054568', 18, 'Shrey@gmail.com', 123, '9.jpg'),
+(32, 'Sarthak', 'male', '22', '8989503913', 19, 'Sarthak@gmail.com', 123, '10.jpg'),
+(33, 'Anchal', 'female', '20', '8871056249', 14, 'anchal@gmail.com', 123, '12.jpg');
 
 -- --------------------------------------------------------
 
@@ -194,15 +162,14 @@ INSERT INTO `donarregistration` (`donar_id`, `name`, `gender`, `age`, `mobile`, 
 -- Table structure for table `donation`
 --
 
-CREATE TABLE IF NOT EXISTS `donation` (
-  `donation_id` int(100) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `donation` (
+  `donation_id` int(100) NOT NULL,
   `camp_id` int(100) NOT NULL,
   `ddate` datetime NOT NULL,
   `units` int(100) NOT NULL,
   `detail` varchar(800) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  PRIMARY KEY (`donation_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=37 ;
+  `email` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `donation`
@@ -225,13 +192,12 @@ INSERT INTO `donation` (`donation_id`, `camp_id`, `ddate`, `units`, `detail`, `e
 -- Table structure for table `gallary`
 --
 
-CREATE TABLE IF NOT EXISTS `gallary` (
+CREATE TABLE `gallary` (
   `camp_id` int(100) NOT NULL,
-  `pic_id` int(100) NOT NULL AUTO_INCREMENT,
+  `pic_id` int(100) NOT NULL,
   `title` varchar(400) NOT NULL,
-  `pic` varchar(800) NOT NULL,
-  PRIMARY KEY (`pic_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=39 ;
+  `pic` varchar(800) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `gallary`
@@ -269,12 +235,11 @@ INSERT INTO `gallary` (`camp_id`, `pic_id`, `title`, `pic`) VALUES
 -- Table structure for table `news`
 --
 
-CREATE TABLE IF NOT EXISTS `news` (
-  `news_id` int(100) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `news` (
+  `news_id` int(100) NOT NULL,
   `title` varchar(100) NOT NULL,
-  `detail` varchar(900) NOT NULL,
-  PRIMARY KEY (`news_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+  `detail` varchar(900) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `news`
@@ -290,8 +255,8 @@ INSERT INTO `news` (`news_id`, `title`, `detail`) VALUES
 -- Table structure for table `requestes`
 --
 
-CREATE TABLE IF NOT EXISTS `requestes` (
-  `req_id` int(100) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `requestes` (
+  `req_id` int(100) NOT NULL,
   `name` varchar(100) NOT NULL,
   `gender` varchar(100) NOT NULL,
   `age` varchar(100) NOT NULL,
@@ -299,9 +264,8 @@ CREATE TABLE IF NOT EXISTS `requestes` (
   `email` varchar(100) NOT NULL,
   `bgroup` int(100) NOT NULL,
   `reqdate` datetime NOT NULL,
-  `detail` varchar(500) NOT NULL,
-  PRIMARY KEY (`req_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+  `detail` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `requestes`
@@ -319,11 +283,10 @@ INSERT INTO `requestes` (`req_id`, `name`, `gender`, `age`, `mobile`, `email`, `
 -- Table structure for table `state`
 --
 
-CREATE TABLE IF NOT EXISTS `state` (
-  `state_id` int(100) NOT NULL AUTO_INCREMENT,
-  `state_name` varchar(100) NOT NULL,
-  PRIMARY KEY (`state_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+CREATE TABLE `state` (
+  `state_id` int(100) NOT NULL,
+  `state_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `state`
@@ -332,7 +295,7 @@ CREATE TABLE IF NOT EXISTS `state` (
 INSERT INTO `state` (`state_id`, `state_name`) VALUES
 (1, 'punjab'),
 (2, 'delhi'),
-(3, 'Andhra Pradesh'),
+(3, 'Madhya Pradesh'),
 (4, 'Bihar'),
 (5, 'Assam');
 
@@ -342,11 +305,11 @@ INSERT INTO `state` (`state_id`, `state_name`) VALUES
 -- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE `users` (
   `username` varchar(100) NOT NULL,
   `pwd` varchar(100) NOT NULL,
   `typeofuser` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `users`
@@ -355,6 +318,135 @@ CREATE TABLE IF NOT EXISTS `users` (
 INSERT INTO `users` (`username`, `pwd`, `typeofuser`) VALUES
 ('manu', 'manu', 'Admin'),
 ('neeru', 'neeru', 'General');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bloodgroup`
+--
+ALTER TABLE `bloodgroup`
+  ADD PRIMARY KEY (`bg_id`);
+
+--
+-- Indexes for table `camp`
+--
+ALTER TABLE `camp`
+  ADD PRIMARY KEY (`camp_id`);
+
+--
+-- Indexes for table `city`
+--
+ALTER TABLE `city`
+  ADD PRIMARY KEY (`city_id`);
+
+--
+-- Indexes for table `contacts`
+--
+ALTER TABLE `contacts`
+  ADD PRIMARY KEY (`row_id`);
+
+--
+-- Indexes for table `donarregistration`
+--
+ALTER TABLE `donarregistration`
+  ADD PRIMARY KEY (`donar_id`);
+
+--
+-- Indexes for table `donation`
+--
+ALTER TABLE `donation`
+  ADD PRIMARY KEY (`donation_id`);
+
+--
+-- Indexes for table `gallary`
+--
+ALTER TABLE `gallary`
+  ADD PRIMARY KEY (`pic_id`);
+
+--
+-- Indexes for table `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`news_id`);
+
+--
+-- Indexes for table `requestes`
+--
+ALTER TABLE `requestes`
+  ADD PRIMARY KEY (`req_id`);
+
+--
+-- Indexes for table `state`
+--
+ALTER TABLE `state`
+  ADD PRIMARY KEY (`state_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bloodgroup`
+--
+ALTER TABLE `bloodgroup`
+  MODIFY `bg_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `camp`
+--
+ALTER TABLE `camp`
+  MODIFY `camp_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `city`
+--
+ALTER TABLE `city`
+  MODIFY `city_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `contacts`
+--
+ALTER TABLE `contacts`
+  MODIFY `row_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `donarregistration`
+--
+ALTER TABLE `donarregistration`
+  MODIFY `donar_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+
+--
+-- AUTO_INCREMENT for table `donation`
+--
+ALTER TABLE `donation`
+  MODIFY `donation_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+
+--
+-- AUTO_INCREMENT for table `gallary`
+--
+ALTER TABLE `gallary`
+  MODIFY `pic_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+
+--
+-- AUTO_INCREMENT for table `news`
+--
+ALTER TABLE `news`
+  MODIFY `news_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `requestes`
+--
+ALTER TABLE `requestes`
+  MODIFY `req_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `state`
+--
+ALTER TABLE `state`
+  MODIFY `state_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
